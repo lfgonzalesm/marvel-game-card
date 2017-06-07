c24dfa1e0e716a1d7256a6f267b0177167027143
lfgonzalesm <lfgonzalesm@users.noreply.github.com>
2017-06-07 16:38:43 -0500

    marvel-game-card
    API, AJAX con JQuery, Promises  y ECMAScript6

---
+++ public/app.js
@@ -0,0 +1,21 @@
+var $ = window.jQuery;
+var marvelApi = window.MarvelApi;
+
+var key = 'ac216bd32a94cf50cbcceb2f71ee590a';
+var api = new MarvelApi(key);
+
+api.findSeries('avengers').then(serie => {
+	var characters = serie.characters.items;
+	var promises = [];
+	for (let character of characters) {
+		let promise = api.getResourceURI(character.resourceURI);
+		promises.push(promise);
+	}
+	return Promise.all(promises);
+}).then(characters => {
+	debugger;
+	console.log(characters);
+}).catch(err => {
+	debugger;
+	console.error(err);
+});
