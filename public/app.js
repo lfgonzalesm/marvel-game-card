198990785fd7896e57a89f3a9d10b7650df18cef
lfgonzalesm <lfgonzalesm@users.noreply.github.com>
2017-06-07 16:51:41 -0500

    marvel-game-card
    Request de una imagen y modificación con JQuery

--- public/app.js
+++ public/app.js
@@ -5,6 +5,9 @@ var key = 'ac216bd32a94cf50cbcceb2f71ee590a';
 var api = new MarvelApi(key);
 
 api.findSeries('avengers').then(serie => {
+	let serieImage = `url(${serie.thumbnail.path}.${serie.thumbnail.extension})`;
+	$('.Layout').css('background-image', serieImage);
+	debugger;
 	var characters = serie.characters.items;
 	var promises = [];
 	for (let character of characters) {
