1e690205fa30a757fb08eb57fa5c3ec94fd98f2e
lfgonzalesm <lfgonzalesm@users.noreply.github.com>
2017-06-07 19:21:44 -0500

    Modificar elementos con ApiData
    Realizaron un filtro para que en arreglo solo se guardara los characters que tenga solo thumbnail

--- public/app.js
+++ public/app.js
@@ -7,7 +7,6 @@ var api = new MarvelApi(key);
 api.findSeries('avengers').then(serie => {
 	let serieImage = `url(${serie.thumbnail.path}.${serie.thumbnail.extension})`;
 	$('.Layout').css('background-image', serieImage);
-	debugger;
 	var characters = serie.characters.items;
 	var promises = [];
 	for (let character of characters) {
@@ -16,8 +15,23 @@ api.findSeries('avengers').then(serie => {
 	}
 	return Promise.all(promises);
 }).then(characters => {
+	return characters.filter(character => {
+		return !!character.thumbnail;
+		debugger;
+	});
+}).then(characters => {
 	debugger;
-	console.log(characters);
+	$('.Card').each((i, item) => {
+		let character = characters[i];
+		let $this = $(item);
+		let $image = $this.find('.Card-image');
+		let $description = $this.find('.Card-description');
+		let $name = $this.find('.Card-name');
+
+		$image.attr('src', `${character.thumbnail.path}.${character.thumbnail.extension}`);
+		$name.text(character.name);
+		$description.text(character.description);
+	});
 }).catch(err => {
 	debugger;
 	console.error(err);
